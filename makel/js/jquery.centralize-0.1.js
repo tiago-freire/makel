d627fdfd94d9d2318fc7946460ec3356d69f0643
freire.tiago@gmail.com <freire.tiago@gmail.com@321f3644-5a30-8059-7b46-b73d7d4d267f>
2010-12-20 15:58:30 +0000

    fixes #1174 Solução implementada: as linhas do arquivo com dia da prova diferente do passado no parâmetro são desconsideradas

--- makel/js/jquery.centralize-0.1.js
+++ makel/js/jquery.centralize-0.1.js
@@ -25,3 +25,35 @@ function centralize(selectorPai, selectorFilhos) {
 		}
 	});
 }
+
+//
+//jQuery.fn.extend({
+//	centralize: function() {
+//		children = this;
+//		parent = children.parent();
+//		children.find('img').load(function() {
+//			var totalWidth = parent.width();
+//
+//			var totalCount = children.length;
+//			var sumWidth = 0;
+//			children.each(function() {
+//				$(this).css('float', 'left');
+//				sumWidth += $(this).outerWidth();
+//			});
+//
+//			var averageWidth = Math.floor(sumWidth / totalCount);
+//
+//			var maxCountByLine = Math.floor(totalWidth / averageWidth);
+//			var count = totalCount > maxCountByLine ? maxCountByLine : totalCount;
+//
+//			var margin = Math.floor((totalWidth - (count * averageWidth)) / (count + 1));
+//
+//			children.css('margin-left', margin + 'px');
+//
+//		}).each(function() {
+//			if(this.complete || ($.browser.msie && parseInt($.browser.version) == 6)) {
+//				$(this).trigger("load");
+//			}
+//		});
+//	}
+//});
